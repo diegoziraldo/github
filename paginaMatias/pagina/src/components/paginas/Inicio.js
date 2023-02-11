9afc63cf0468f3749e7ca4c85e9b39a235235208
diegoziraldo <ziraldodiego@gmail.com>
2023-02-11 11:13:44 -0300

    OK

--- paginaMatias/pagina/src/components/paginas/Inicio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Inicio() {
-  return (
-    <p>Inicio</p>
-  )
-}
-
-export default Inicio
--- paginaMatias/pagina/src/components/paginas/Premios.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Premios() {
-  return (
-    <p>Premios</p>
-  )
-}
-
-export default Premios
