f0c9b65810b6a9b02beabedb2b1f18918f2bada4
diegoziraldo <ziraldodiego@gmail.com>
2023-02-12 11:32:52 -0300

    120223

---
+++ MERN/MERN_2/backend/src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const app = require('./app')
+require ('./database')
+
+const main = async()=>{
+    
+    await app.listen(3000)
+        console.log('Server on port 3000');
+
+}
+
+
+main()
+
