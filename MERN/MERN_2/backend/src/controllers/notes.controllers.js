334a2b1be770fb4f8cc265186303da89354699dc
diegoziraldo <ziraldodiego@gmail.com>
2023-02-12 20:34:21 -0300

    120223

--- MERN/MERN_2/backend/src/controllers/notes.controllers.js
+++ MERN/MERN_2/backend/src/controllers/notes.controllers.js
@@ -15,19 +15,34 @@ notesControllers.getNotes = async (req,res)=>{
 notesControllers.createNote = async(req,res)=>{
     const {title, content, autor, date} = req.body;
         const newNote = new Note({
-            title:title,
-            content:content,
-            autor:autor,
-            date:date
+            title,
+            content,
+            author,
+            date
         })
         await newNote.save();
     res.json({message: 'Nota guardada'})
 }
-notesControllers.getNote = (req,res)=>res.json({title: 'Hola'})
 
-notesControllers.updateNote = (req,res)=>res.json({message: 'Note updated'})
+notesControllers.getNote = async(req,res)=>{
+    const note = await Note.findById(req.params.id)
+    console.log(note);
+    res.json(note.content)
+}
 
-notesControllers.deleteNote = (req,res)=>res.json({message: 'Note deleted'})
+notesControllers.updateNote = async(req,res)=>{
+    const {title, content, author} = req.body;
+    await Note.findByIdAndUpdate({_id: req.params.id},{
+        title,
+        content,
+        author,
+    })
+    res.json({message: 'Note updated'})
+}
 
+notesControllers.deleteNote = async(req,res)=>{
+    await Note.findByIdAndDelete(req.params.id)
+    res.json({message: 'Note deleted'})
+}
 
 module.exports = notesControllers;
--- MERN/MERN_2/backend/src/controllers/users.controllers.js
+++ MERN/MERN_2/backend/src/controllers/users.controllers.js
@@ -1,11 +1,23 @@
-const userControllers = {
+const userControllers = {}
 
+const User = require('../models/User')
+
+userControllers.getUsers = async(req,res)=>{
+    const users = await User.find();
+    res.json(users);
 }
 
-userControllers.getUsers = (req,res)=>res.send('GET - Users Route');
-userControllers.createUser = (req,res)=>res.send('GET - Users Route');
-userControllers.deleteUser = (req,res)=>res.send('GET - Users Route');
+userControllers.createUser = async(req,res)=>{
+    const {username} = req.body;
+    const newUser = new User({username})
+    await newUser.save();
+    res.json('User Create');
+}
 
+userControllers.deleteUser = async(req,res)=>{
+    await User.findOneAndDelete(req.params.id)
+    res.json('User deleted');
+}
 
 
 module.exports = userControllers;
