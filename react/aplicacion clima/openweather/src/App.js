3e67b8c67164b04bdafb2c15d1afb3d180ee455f
diegoziraldo <ziraldodiego@gmail.com>
2023-02-22 11:26:18 -0300

    220223

---
+++ react/aplicacion clima/openweather/src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/App.css';
+import Navbar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/aplicacion clima/openweather/src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+
+const NavBar = ()=>{
+    return(
+        <nav className="navbar bg-dark-light mb-5">
+            <div className="container-fluid"> 
+                <h3>Prediccion meteorolgica</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
