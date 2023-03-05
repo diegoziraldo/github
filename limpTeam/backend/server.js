bffe88c11bb0cd1d683802c45b4d3c698730012d
diegoziraldo <ziraldodiego@gmail.com>
2023-03-05 20:11:58 -0300

    050323

--- limpTeam/backend/server.js
+++ limpTeam/backend/server.js
@@ -1,5 +1,17 @@
-const express = require ('express');
-const app = express();
-const port = 8080;
+require('dotenv').config();
+const app = require ('./app');
+const connectDB = require('./db/mongodb')
+const {appConfig, dbConfig} = require('./config')
 
-app.listen(port,()=>console.log(`Listen on port ${port}`));
+
+const initApp = async(appConfig, dbConfig)=>{
+    try {
+        await connectDB(dbConfig)
+        app.listen(appConfig.port,()=>console.log(`Listen on port ${appConfig.port}`));
+    } catch (error) {
+            console.error(e)    
+            process.exit(0)
+    }
+}
+
+initApp(appConfig, dbConfig)
