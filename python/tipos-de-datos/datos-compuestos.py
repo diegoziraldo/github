c5730da1b2f6a2f7f62eaa9063cd03412cc6a0f9
diegoziraldo <ziraldodiego@gmail.com>
2023-03-06 13:47:41 -0300

    060323

---
+++ python/tipos-de-datos/datos-compuestos.py
@@ -0,0 +1,25 @@
+#ACA ESTAMOS CREANDO UNA LISTA
+lista = ['Diego', True, 1.80]   #Aca estamos creando una lista
+print(lista[0])
+
+
+#ACA ESTAMOS CREANDO UNA TUPLA
+tupla = ('Diego', True, 1.80)   #Aca estamos creando una tupla, la diferencia es que no se pueden modificar.
+print(tupla[2])
+lista[2] = '1.75'   #De esta manera estamos modificando la lista
+print(lista)
+
+
+#CREANDO UN CONJUNTO (set)
+conjunto = {'Diego Ziraldo', True, 1.70} #En un conjunto no pueden haber datos duplicados. Tampoco se puede acceder al elemento por su numero de indice
+print(conjunto)
+
+
+#ACA ESTAMOS CREANDO UN DICCIONARIO (dict)
+diccionario = {
+    'nombre':'Diego',
+    'apellido':'Ziraldo',
+    'telefono':'1515151515'
+
+}
+print(diccionario['nombre'])
--- python/variables/variables.py
+++ python/variables/variables.py
@@ -5,14 +5,16 @@ c = a + b
 print(c)
 
 
-nombre = 'Diego'
+nombre = 'Dieg'
 
 print(nombre)
 
 nombre = 'Diego'
 bienvenida = f'Hola {nombre} como estas?' #De esta manera concatenamos un string con otras variables.
 
-del bienvenida #De esta manera borramos el contenido de una variable.
+#del bienvenida #De esta manera borramos el contenido de una variable.
 
-print(bienvenida)
+print('como' in bienvenida) #De esta manera comprobamos si la palabra existe dentro de la variable 'bienvenida'
+print('Diego' not in bienvenida) #De esta manera comprobamos si la palabra no existe dentro de la variable 'bienvenida'
+
 
